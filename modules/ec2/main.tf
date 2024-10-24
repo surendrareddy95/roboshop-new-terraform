03c7a1a65bb740c895826cb65aa7df39212e5b81
surendra <surendraredddyalamuru01@gmail.com>
2024-10-24 12:33:13 +0530

    roboshop new terraform

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.id
   instance_type = "t3.small"
-  vpc_security_group_ids = data.aws_security_group.sg.id
+  vpc_security_group_ids = ["data.aws_security_group.sg.id"]
   tags = {
     Name = "test-${var.env}"
   }
