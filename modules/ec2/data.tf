3942ab1e3fc09426d134ab022010f4fb90d17716
surendra <surendraredddyalamuru01@gmail.com>
2024-12-31 13:03:07 +0530

    roboshop-instances

--- modules/ec2/data.tf
+++ modules/ec2/data.tf
@@ -1,14 +1,14 @@
-#data "aws_ami" "ami" {
-#  most_recent = true
-#  name_regex  = "Centos-8-DevOps-Practice"
-#  owners      = ["973714476881"]
-#}
-
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "RHEL-9-DevOps-Practice"
-  owners      = ["396608809975"]
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
 }
+
+#data "aws_ami" "ami" {
+#  most_recent = true
+#  name_regex  = "RHEL-9-DevOps-Practice"
+#  owners      = ["396608809975"]
+#}
 
 data "aws_security_group" "sg" {
   name = "allow-all"
