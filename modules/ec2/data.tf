6ab8ebcaa22ad6468e924938001f81bf53967c86
surendra <surendraredddyalamuru01@gmail.com>
2025-01-02 15:15:42 +0530

    tools setup

--- modules/ec2/data.tf
+++ modules/ec2/data.tf
@@ -1,14 +1,8 @@
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["973714476881"]
+  name_regex  = "RHEL-9-DevOps-Practice"
+  owners = ["396608809975"]
 }
-
-#data "aws_ami" "ami" {
-#  most_recent = true
-#  name_regex  = "RHEL-9-DevOps-Practice"
-#  owners      = ["396608809975"]
-#}
 
 data "aws_security_group" "sg" {
   name = "allow-all"
