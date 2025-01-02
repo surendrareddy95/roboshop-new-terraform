2fdd798141ead0ac6761f0cc304d50581e1db78d
surendra <surendraredddyalamuru01@gmail.com>
2025-01-02 17:31:46 +0530

    roboshop-terraform

--- modules/ec2/data.tf
+++ modules/ec2/data.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "RHEL-9-DevOps-Practice"
-  owners = ["396608809975"]
+  owners = ["973714476881"]
 }
 
 data "aws_security_group" "sg" {
