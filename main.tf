f62239a278eb104e62f1931503b944cf878bc633
surendra <surendraredddyalamuru01@gmail.com>
2025-05-22 12:33:31 +0530

    roboshop-terraform

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "db-instances" {
   for_each = var.db_instances
-  source = "./modules/ec2"
+  source = "modules/ec2"
   env = var.env
   zone_id = var.zone_id
   component_name = each.key
@@ -16,7 +16,7 @@ module "db-instances" {
 
 module "app-instances" {
   for_each = var.app_instances
-  source = "./modules/ec2"
+  source = "modules/ec2"
   zone_id = var.zone_id
   env = var.env
   component_name = each.key
@@ -28,7 +28,7 @@ module "app-instances" {
 
 module "web-instances" {
   for_each = var.web_instances
-  source = "./modules/ec2"
+  source = "modules/ec2"
   env = var.env
   zone_id = var.zone_id
   component_name = each.key
